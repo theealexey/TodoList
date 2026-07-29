a75bfc43f105bae25e1f75efd480a3f9487d396a
theealexey <theealexey@gmail.com>
2026-07-29 22:01:02 +0300

    chore: initialize Xcode project

---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import TodoList
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
